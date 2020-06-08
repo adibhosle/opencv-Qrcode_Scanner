ee24dab595a9ccd76096b53b12b71ae611692ccc
Aditya Bhosle <39487795+adibhosle@users.noreply.github.com>
2020-06-08 10:10:17 +0530

    Update project.py

--- NewScanner/project.py
+++ NewScanner/project.py
@@ -3,7 +3,7 @@ import numpy as np
 from pyzbar.pyzbar import decode
 
 # img = cv2.imread("code.png")
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1) #if using laptop camera use cv2.VideoCapture(0) 
 cap.set(3, 640)
 cap.set(4, 480)
 
